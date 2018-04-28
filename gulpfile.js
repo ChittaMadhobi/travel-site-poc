ae99e6a30e966eeb9a4f367d2627e0064a9779c1
Jit <sarbojit.project@gmail.com>
2018-04-28 10:23:57 -0700

    Footer completed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
 /* We impoert the files we just created in this main gulp file */
 require('./gulp/tasks/styles');   // All that is style related
 require('./gulp/tasks/watch');    // The main watch related tasks
+require('./gulp/tasks/sprites');
